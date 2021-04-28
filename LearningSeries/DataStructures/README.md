200fcef1c25b5e297c5c13fe2901897484653e8f
Pawan Nirpal <pawannirpal@gmail.com>
2021-04-28 13:31:10 +0530

    Lazy prop added to seg-tree

---
+++ LearningSeries/DataStructures/README.md
@@ -0,0 +1,9 @@
+# Data Structures 
+
+> Segment Tree + Lazy propogration 
+> Sparse table
+> Fenwik Tree 
+> Union Find (DSU)
+> Tries 
+> Suffix array 
+> Suffix Automata 
--- LearningSeries/DataStructures/SegmentTrees.cpp
+++ LearningSeries/DataStructures/SegmentTrees.cpp
@@ -13,8 +13,7 @@ typedef long double ld;
 #define DebugCase(i,x) cout<<"Case #"<<i<<": "<<x<<'\n'
 #define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define Status(b) (cout<<(b?"YES\n":"NO\n"));
-#define Print(x) cout<<x
-#define Input(x) cin>>x
+
 
 inline int Merge(vector<int>&tree,int left,int right){
     return min(tree[left],tree[right]);
@@ -28,7 +27,7 @@ void BuildTree(int tree_indx,int seg_left,int seg_right,vector<int>&a,vector<int
     int mid = ((seg_left + seg_right)>>1);
     BuildTree(2*tree_indx,seg_left,mid,a,tree);
     BuildTree(2*tree_indx+1,mid+1,seg_right,a,tree);
-    tree[tree_indx] = min(tree[2*tree_indx],tree[2*tree_indx+1]);
+    tree[tree_indx] = tree[2*tree_indx]+tree[2*tree_indx+1];
 }
 
 void PointUpdate(int tree_indx,int seg_left,int seg_right,vector<int>&tree,int updt_indx,int updt_val){
@@ -42,40 +41,76 @@ void PointUpdate(int tree_indx,int seg_left,int seg_right,vector<int>&tree,int u
     }else{
         PointUpdate(2*tree_indx+1,mid+1,seg_right,tree,updt_indx,updt_val);
     }
-    tree[tree_indx] = min(tree[2*tree_indx],tree[2*tree_indx+1]);
+    tree[tree_indx] = tree[2*tree_indx]+tree[2*tree_indx+1];
 }
 
-
-int QueryRange(int tree_indx,int seg_left,int seg_right,vector<int>&tree,int query_left,int query_right){
-    if(seg_left>=query_left and seg_right<=query_right) return tree[tree_indx];
-    if(query_right<seg_left or query_left>seg_right) return INT_MAX;
+void RangeUpdateLazy(int tree_indx,int seg_left,int seg_right,vector<int>&tree,vector<int>&lazy,int updt_left,int updt_right,int val){
+    if(lazy[tree_indx]!=0){
+        tree[tree_indx]+=((seg_right-seg_left+1)*(lazy[tree_indx]));
+        if(seg_left!=seg_right){
+            lazy[2*tree_indx]+=lazy[tree_indx];
+            lazy[2*tree_indx+1]+=lazy[tree_indx];
+        }
+        lazy[tree_indx] =0;
+    }
+    if(updt_left>seg_right or updt_right<seg_left) return;
+    if(seg_left>=updt_left and seg_right<=updt_right){
+        tree[tree_indx]+=((seg_right-seg_left+1)*(val));
+        if(seg_left!=seg_right){
+            lazy[2*tree_indx]+=val;
+            lazy[2*tree_indx+1]+=val;
+        }
+        return;
+    }
     int mid = ((seg_left + seg_right)>>1);
-    int left = QueryRange(2*tree_indx,seg_left,mid,tree,query_left,query_right);
-    int right = QueryRange(2*tree_indx+1,mid+1,seg_right,tree,query_left,query_right);
-    return min(left,right);
+    RangeUpdateLazy(2*tree_indx,seg_left,mid,tree,lazy,updt_left,updt_right,val);
+    RangeUpdateLazy(2*tree_indx+1,mid+1,seg_right,tree,lazy,updt_left,updt_right,val);
+    tree[tree_indx] = tree[2*tree_indx]+tree[2*tree_indx+1];
 }
 
+int RangeQuery(int tree_indx,int seg_left,int seg_right,vector<int>&tree,vector<int>&lazy,int query_left,int query_right){
+    if(lazy[tree_indx]!=0){
+        tree[tree_indx]+=((seg_right-seg_left+1)*(lazy[tree_indx]));
+        if(seg_left!=seg_right){
+            lazy[2*tree_indx]+=lazy[tree_indx];
+            lazy[2*tree_indx+1]+=lazy[tree_indx];
+        }
+        lazy[tree_indx] =0;
+    }
+    if(seg_left>=query_left and seg_right<=query_right) return tree[tree_indx];
+    if(query_right<seg_left or query_left>seg_right) return 0;
+    int mid = ((seg_left + seg_right)>>1);
+    int left = RangeQuery(2*tree_indx,seg_left,mid,tree,lazy,query_left,query_right);
+    int right = RangeQuery(2*tree_indx+1,mid+1,seg_right,tree,lazy,query_left,query_right);
+    return left+right;
+}
+
+
+void LazyPropDemo(){
+    int n,q;
+    cin>>n>>q;
+    vector<int>a(n+1,0);
+    for(int i =1;i<=n;i++) cin>>a[i];
+    vector<int>tree(4*n+5,0),lazy(4*n+5,0);
+    BuildTree(1,1,n,a,tree);
+    while(q--){
+        char c;
+        cin>>c;
+        if(c=='q'){//'query'
+            int l,r;
+            cin>>l>>r;
+            cout<<RangeQuery(1,1,n,tree,lazy,l,r)<<endl;
+        }else{//'update'
+            int l,r,val; // add val to all elemnts in range [l,r]
+            cin>>l>>r>>val;
+            RangeUpdateLazy(1,1,n,tree,lazy,l,r,val);
+        }
+    }
+  
+}
 
 int main(){
   FastIO;
-  int n,q;
-  cin>>n>>q;
-  vector<int>a(n+1,0);
-  for(int i =1;i<=n;i++) cin>>a[i];
-  vector<int>tree(4*n+5,0);
-  BuildTree(1,1,n,a,tree);
-  while(q--){
-      char c;
-      cin>>c;
-      if(c=='q'){
-          int l,r;
-          cin>>l>>r;
-          cout<<QueryRange(1,1,n,tree,l,r)<<'\n';
-      }else{
-          int indx,val;
-          cin>>indx>>val;
-          PointUpdate(1,1,n,tree,indx,val);
-      }
-  }
+  LazyPropDemo();
   return 0;
 } 
